52fb184db8c822cc1840ca20c9cf2e0fdc72a46d
Cheese479 <181613388+Cheese479@users.noreply.github.com>
2025-12-30 00:22:31 -0600

    Enhance command parser with new commands
    Updated command handling to include 'ver' for version display, changed 'version' to 'ver' in help text, and added 'echo' and 'calc' command functionalities.

--- src/kernel/shell/parser.c
+++ src/kernel/shell/parser.c
@@ -23,11 +23,13 @@ void parser(void) {
         cmd[3] == 'a' && cmd[4] == 'r' && cmd[5] == '\0') {
 
         clear_screen();
+        return;
     }
 
     //ver
     if (cmd[0] == 'v' && cmd[1] == 'e' && cmd[2] == 'r' && cmd[3] == '\0') {
         print("Jinux v0.01 by James Baum published with the MIT license on December 21st 2025\n");
+        return;
     }
 
     //help
@@ -35,13 +37,75 @@ void parser(void) {
         print("Available commands:\n");
         print("clear - clear the screen\n");
         print("shutdown - halt the cpu\n");
-        print("version - display the version of Jinux\n");
+        print("ver - display the version of Jinux\n");
         print("help - display this help message\n");
         print("about - display information about Jinux!\n");
+        print("echo - print text to the screen!\n");
+        print("calc - simple calculator (format: calc A + B) (note: doesnt work if the result is greater than 9 and only works with integers)\n");
+        return;
     }
 
     //about
     if (cmd[0] == 'a' && cmd[1] == 'b' && cmd[2] == 'o' && cmd[3] == 'u' && cmd[4] == 't' && cmd[5] == '\0') {
         print("Jinux is an OS by me (James Baum) and aims to one day in the future become something great!\nBut right now it's not much, you can contribute by going to https://github.com/Cheese479/Jinux\n");
+        return;
+    }
+    //echo
+    if (cmd[0] == 'e' && cmd[1] == 'c' && cmd[2] == 'h' &&
+        cmd[3] == 'o' && cmd[4] == ' ') {
+        //print everything after "echo "
+        int i = 5;
+        while (cmd[i] != '\0') {
+            print((const char[]){cmd[i], '\0'});
+            i++;
+        }
+        print("\n");
+        return;
+    }
+    //calc
+    if (cmd[0] == 'c' && cmd[1] == 'a' && cmd[2] == 'l' &&
+        cmd[3] == 'c' && cmd[4] == ' ') {
+        int a = cmd[5] - '0';
+        int b = cmd[9] - '0'; 
+        int x;
+        if (cmd[7] == '+') {
+            int x = a + b;
+            char out[2];
+            out[0] = x + '0';
+            out[1] = '\0';
+            print(out);
+            print("\n");
+        }
+        if (cmd[7] == '-') {
+            int x = a - b;
+            char out[2];
+            out[0] = x + '0';
+            out[1] = '\0';
+            print(out);
+            print("\n");
+        }
+        if (cmd[7] == '*') {
+            int x = a * b;
+            char out[2];
+            out[0] = x + '0';
+            out[1] = '\0';
+            print(out);
+            print("\n");
+        }
+        if (cmd[7] == '/') {
+            int x = a / b;
+            char out[2];
+            out[0] = x + '0';
+            out[1] = '\0';
+            print(out);
+            print("\n");
+        }
+        return;
+    }
+    else {
+        //print unknown command message
+        if (!(cmd[0] == '\0')) {
+            print("Unknown command. Type 'help' for a list of commands.\n");
+        }
     }
 }
